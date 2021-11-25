ca6396c937558da4c7d0af466a2eb4d29d02f9a5
Matheus Costa <matheus271200.ms@gmail.com>
2021-11-25 10:57:58 -0300

    perf: using conclude use case to store and call microservice

---
+++ server/src/useCases/ConcludeCourseUseCase.ts
@@ -0,0 +1,31 @@
+import { ConcludeCourseDTO } from "../DTO/ConcludeCourseDTO";
+import { ICourse } from "../models/ICourse";
+import { Course } from "../models/implementations/Course";
+
+class ConcludeCourseUseCase {
+  private course: ICourse;
+  constructor(private producer: Producer) {
+    this.course = Course.getInstance();
+  }
+
+  async execute({ name, course, grade }: ConcludeCourseDTO): Promise<void> {
+    this.course.add({ name, course, grade });
+
+    const message = {
+      user: { id: 1, name },
+      course,
+      grade,
+    };
+
+    await this.producer.send({
+      topic: "issue-certificate",
+      messages: [
+        {
+          value: JSON.stringify(message),
+        },
+      ],
+    });
+  }
+}
+
+export { ConcludeCourseUseCase };
