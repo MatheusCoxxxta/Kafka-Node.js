6160d57530383f3df7d973af96ef78119f3bdb51
Matheus Costa <matheus271200.ms@gmail.com>
2021-11-25 10:59:56 -0300

    feat: applying singleton to keep data

---
+++ server/src/models/implementations/Course.ts
@@ -0,0 +1,32 @@
+import { ICourse } from "../ICourse";
+
+export type CourseType = {
+  name: string;
+  course: string;
+  grade: number;
+};
+
+class Course implements ICourse {
+  private course: CourseType[];
+
+  private static INSTANCE: Course;
+
+  public static getInstance(): Course {
+    if (!Course.INSTANCE) {
+      Course.INSTANCE = new Course();
+    }
+    return Course.INSTANCE;
+  }
+
+  constructor() {
+    this.course = [];
+  }
+
+  public add({ name, course, grade }: CourseType): void {
+    this.course.push({ name, course, grade });
+
+    console.log(this.course);
+  }
+}
+
+export { Course };
