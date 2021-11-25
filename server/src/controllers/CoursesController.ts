c43e97c513e129b18d071cce6b9b2a7b1543a097
Matheus Costa <matheus271200.ms@gmail.com>
2021-11-25 10:59:36 -0300

    feat: calling metod concludeCourseUseCase

--- server/src/controllers/CoursesController.ts
+++ server/src/controllers/CoursesController.ts
@@ -1,14 +1,13 @@
 import { Request, Response } from "express";
-import { GenerateCertificateUseCase } from "../useCases/GenerateCertificateUseCase";
+import { ConcludeCourseUseCase } from "../useCases/ConcludeCourseUseCase";
 
 class CoursesController {
   public async concludeCourse(request: Request, response: Response) {
     const { name, course, grade } = request.body;
     const { producer } = request;
 
-    const generateCertificateUseCase = new GenerateCertificateUseCase(producer);
-
-    await generateCertificateUseCase.execute({ name, course, grade });
+    const concludeCourseUseCase = new ConcludeCourseUseCase(producer);
+    await concludeCourseUseCase.execute({ name, course, grade });
 
     return response.json({ ok: true });
   }
