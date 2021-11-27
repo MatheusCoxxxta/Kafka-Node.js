c51f5c910f6ec4b76ec6e578bccefb5f5263f36c
Matheus Costa <matheus271200.ms@gmail.com>
2021-11-26 21:33:09 -0300

    feat: declaring method return to string

--- server/src/models/ICourse.ts
+++ server/src/models/ICourse.ts
@@ -1,5 +1,5 @@
 import { ConcludeCourseDTO } from "../dto/ConcludeCourseDTO";
 
 export interface ICourse {
-  add({ name, course, grade }: ConcludeCourseDTO): void;
+  add({ name, course, grade }: ConcludeCourseDTO): string;
 }
