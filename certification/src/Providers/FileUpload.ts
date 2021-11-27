0b67d4ae07439d47926ceabde91e14f5aaac680f
Matheus Costa <matheus271200.ms@gmail.com>
2021-11-27 18:00:09 -0300

    feat: creating s3 upload provider

---
+++ certification/src/Providers/FileUpload.ts
@@ -0,0 +1,38 @@
+import AWS from "aws-sdk";
+import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+interface ICredentials {
+  accessKeyId: string;
+  secretAccessKey: string;
+}
+
+class FileUpload {
+  async execute(fileName: string): Promise<string> {
+    const s3 = new AWS.S3({
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      } as ICredentials,
+    });
+
+    const body = fs.readFileSync(`tmp/${fileName}`);
+
+    const params = {
+      Bucket: "certificates-application",
+      Key: fileName,
+      Body: body,
+      acl: "public-read",
+    };
+
+    const options = { partSize: 10 * 1024 * 1024, queueSize: 1 };
+
+    const response = await s3.upload(params, options).promise();
+
+    return response.Location;
+  }
+}
+
+export { FileUpload };
