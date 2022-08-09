e1ef54250f5a8bdbcc696745b163b68fe9f2cd9a
Matheus Costa <matheus271200.ms@gmail.com>
2022-08-09 01:54:45 -0300

    feat: creating listeners init

--- certification/src/index.ts
+++ certification/src/index.ts
@@ -1,3 +1,11 @@
-import { run } from "./configs/kafka";
+import broker from "./configs/broker";
+
+import CreateController from "./controllers/Create";
+
+const createController = new CreateController(broker);
+
+async function run() {
+  await createController.handle();
+}
 
 run().catch(console.error);
